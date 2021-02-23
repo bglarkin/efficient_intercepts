375d8fc3fe4dac3e5d04bfb5412a7fc0de5c7356
Beau Larkin <55502481+bglarkin@users.noreply.github.com>
2021-02-23 11:38:58 -0700

    Data pulled successfully from BQ database

--- raw_code_sandbox.R
+++ raw_code_sandbox.R
@@ -16,6 +16,7 @@
 # 
 # Keeping these keys out of this notebook is important for security.
 
+
 #### Tools ####
 # ——————————————————————————————————
 
@@ -64,3 +65,90 @@ register_google(key = mapKey)
 bq_auth(path = "/content/mpg-data-warehouse-api_key-master.json")
 Sys.setenv(BIGQUERY_TEST_PROJECT = "mpg-data-warehouse")
 billing <- bq_test_project()
+
+# Global functions and styles
+# ————————————————————————————————————————
+# Load supplemental.R, a text file from Google Drive.
+# This file contains theme and style elements for plotting.
+source("https://drive.google.com/uc?id=1EYkUWlqsH6g-rqiu27QQsUvl4KaNPzzt") 
+
+
+#### Source data ####
+# ——————————————————————————————————
+
+# Point-intercept species data
+# ————————————————————————————————————————
+# Make raw data available locally by pulling from the MPG Data Warehouse (`gpint_pull_df`), 
+# and then pre-process to create two objects that will be joined with metadata and used for analysis:
+# 1. Species detections in long-form with selected fields only: `gpint_spe_df`
+# 1. `NA` values must be replaced with `360`, the code for "no vegetation", to preserve matrix dimensions and accurately reflect sampling effort in rarefaction analysis. These will be removed after transformation to a sparse matrix using `select()`
+# 1. Height data with selected fields only: `gpint_ht_df`
+
+spe_pull_sql <-
+  "
+  SELECT *
+  FROM `mpg-data-warehouse.vegetation_point_intercept_gridVeg.gridVeg_point_intercept_vegetation`
+  WHERE year = 2016
+  "
+spe_pull_bq <- bq_project_query(billing, spe_pull_sql)
+spe_pull_tb <- bq_table_download(spe_pull_bq)
+spe_pull_df <- as.data.frame(spe_pull_tb) %>% glimpse()
+# Note that in `spe_pull_df`, with 200 intercepts per grid point, the total number of records indicated here should not be possible. Under investigation, I found that 7 grid points contain only 199 records, so no correction to the data is possible. For this analysis, a small number of missing records should not affect the interpretation.
+
+# 1. Transform species detections to long form
+# Replace NA values with 360
+spe_df <- 
+  spe_pull_df %>% 
+  select(grid_point, transect_point, starts_with("intercept")) %>% 
+  pivot_longer(starts_with("intercept"), names_to = "intercept", values_to = "key_plant_species") %>% 
+  replace_na(list(key_plant_species = 360)) %>% 
+  glimpse()
+
+# 2. Produce df of height data
+ht_df <-
+  spe_pull_df %>% 
+  select(grid_point, transect_point, height_intercept_1) %>% 
+  glimpse()
+
+
+# Point-intercept ground cover data
+# ————————————————————————————————————————
+grcov_pull_sql <-
+  "
+  SELECT *
+  FROM `mpg-data-warehouse.vegetation_point_intercept_gridVeg.gridVeg_point_intercept_ground`
+  WHERE year = 2016
+  "
+grcov_pull_bq <- bq_project_query(billing, grcov_pull_sql)
+grcov_pull_tb <- bq_table_download(grcov_pull_bq)
+grcov_pull_df <- 
+  as.data.frame(grcov_pull_tb) %>% 
+  select(grid_point, intercept_ground_code) %>% 
+  glimpse()
+
+
+# Vegetation species metadata
+# ————————————————————————————————————————
+spe_meta_sql <-
+  "
+  SELECT key_plant_species, key_plant_code, plant_native_status, plant_life_cycle, plant_life_form, plant_name_sci, plant_name_common
+  FROM `mpg-data-warehouse.vegetation_species_metadata.vegetation_species_metadata`
+  "
+spe_meta_bq <- bq_project_query(billing, spe_meta_sql)
+spe_meta_tb <- bq_table_download(spe_meta_bq)
+spe_meta_df <- as.data.frame(spe_meta_tb) %>% glimpse()
+
+
+# Grid point metadata
+# ————————————————————————————————————————
+gp_meta_sql <- 
+  "
+  SELECT *
+  FROM `mpg-data-warehouse.grid_point_summaries.location_position_classification`
+  "
+gp_meta_bq <- bq_project_query(billing, gp_meta_sql)
+gp_meta_tb <- bq_table_download(gp_meta_bq)
+gp_meta_df <- as.data.frame(gp_meta_tb) %>% glimpse()
+
+
+
